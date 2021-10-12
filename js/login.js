4af942fff8c2a184eff0497d09a37abc7dc49900
Maxi Chan <maximilianochan8@gmail.com>
2021-10-12 03:44:33 -0300

    beta v1.0

---
+++ js/login.js
@@ -0,0 +1,66 @@
+class Usuario{
+    constructor(usuario, contrasena, mail, telefono){
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+        this.mail = mail;
+        this.telefono = telefono;
+    }
+    getUsuario(){
+        return this.usuario;
+    }
+    getContrasena(){
+        return this.contrasena;
+    }
+    getMail(){
+        return this.mail;
+    }
+    getTelefono(){
+        return this.telefono;
+    }
+}
+
+const usuarios = [];
+
+usuarios.push(new Usuario("maxi","chan", "hola@gmail.com", "123456"));
+usuarios.push(new Usuario("jose","chan", "chau@gmail.com", "123456"));
+usuarios.push(new Usuario("carlos","chan", "adios@gmail.com", "123456"));
+
+
+// CHEQUEO QUE ESTEN BIEN CREADOS LOS USUARIOS
+console.log(usuarios[0].usuario);
+console.log(usuarios[1].usuario);
+console.log(usuarios[2].usuario);
+
+
+
+function validar(){
+    let x = document.forms["form_login"]["inputuser"].value;
+    let y = document.forms["form_login"]["inputpassword"].value;
+    let contador = 0;
+
+    if(x == "" || y == ""){
+        alert("Se deben rellenar todos los espacios");
+        return false;
+    }
+
+    for(let i = 0; i < usuarios.length; i++){
+        if(x == usuarios[i].getUsuario() && y == usuarios[i].getContrasena()){
+            alert("Bienvenido"+" "+usuarios[i].getUsuario())
+            return false; //PARA NO REDIRIGIR
+        }
+        //ESTE METODO REPITE EL ALERT PARA CADA ITERACION DEL FOR SOBRE USUARIOS
+        // else{
+        //     alert("El usuario y/o contrseñan no existen/coinciden");
+        //     return false;
+        // }
+        
+        else
+            contador++;
+
+        if(contador == usuarios.length){
+            alert("El usuario y/o contrseñan no existen/coinciden");
+            return false;
+        }
+    }
+
+}
